af855b9062a090aee1a019d386d97610fb2c88e9
admin <admin@Bcalab3-m16>
2022-06-13 09:21:08 +0530

    comit1

---
+++ CaseToolsExam/src/Pendrive_121.java
@@ -0,0 +1,16 @@
+
+public class Pendrive_121 {
+
+	int size;
+	
+	void Pendrive (int size)
+	{
+		this.size= size;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
